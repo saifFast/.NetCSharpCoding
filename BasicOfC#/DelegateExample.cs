d9664c019ad11f7216c4e8fd80900faf6ae1b8d3
saifFast <saifullahhakro@gmail.com>
2024-09-24 11:16:45 +0500

    Example of Usage of Delegate in C#

---
+++ BasicOfC#/DelegateExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BasicOfC_
+{
+    internal class DelegateExample
+    {
+        public void Start()
+        {
+            MyDelegate @delegate = GreetMorning;
+            @delegate("Saif");
+            @delegate = PrintName;
+            @delegate("Saif");
+        }
+
+        public delegate void MyDelegate(string name);
+
+        public static void GreetMorning(string name)
+        {
+            Console.WriteLine("Hi : " + name);
+        }
+
+        public static void PrintName(string nameOfPerson)
+        {
+            Console.WriteLine($"{nameOfPerson}");
+        }
+    }
+}
--- BasicOfC#/Program.cs
+++ BasicOfC#/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BasicOfC_
 {
@@ -10,6 +6,11 @@ namespace BasicOfC_
     {
         static void Main(string[] args)
         {
+            
+
+            Console.ReadLine();
         }
+
+       
     }
 }
