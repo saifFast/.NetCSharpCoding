05b0f2e65febfc0c983a39e146d6b67d81a0d039
saifFast <saifullahhakro@gmail.com>
2024-10-15 15:05:25 +0500

    Added Example of Singleton With Lock and Factory Pattern

---
+++ BasicOfC#/Design Patterns/FactoryPattern.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOfC_.Design_Patterns
+{
+    internal class FactoryPattern
+    {
+        public IElement MakeElement(string elementName)
+        {
+            switch (elementName)
+            {
+                case nameof(SodioumElement):
+                    return new SodioumElement();
+                case nameof(HydrogenElement):
+                    return new HydrogenElement();
+                default:
+                    throw new ArgumentException();
+            }
+        }
+    }
+
+    public interface IElement
+    {
+        string Name { get; }
+    }
+
+    public class SodioumElement : IElement
+    {
+        public string Name => "SodioumElement";
+    }
+
+    public class HydrogenElement : IElement
+    {
+        public string Name => "HydrogenElement";
+    }
+
+
+
+}
---
+++ BasicOfC#/Design Patterns/SingletonWithLock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace BasicOfC_.Design_Patterns
+{
+    public class SingletonWithLock
+    {
+        private static SingletonWithLock instance = null;
+        private static object lockInstance = new object();
+
+        private SingletonWithLock()
+        {
+
+        }
+
+        public static SingletonWithLock Instance
+        {
+            get
+            {
+                lock (lockInstance)
+                {
+                    if (instance == null)
+                    {
+                        instance = new SingletonWithLock();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public void ShowMessage()
+        {
+            Console.WriteLine("Hey");
+        }
+    }
+}
