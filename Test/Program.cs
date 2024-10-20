2ffe0cd017016a3c6e2c31eea0a62291b13f2ced
saifFast <saifullahhakro@gmail.com>
2024-10-20 12:33:47 +0500

    Lambda functions and delegates and Generic Classes

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,12 +1,40 @@
 ﻿
+using System;
+
 namespace Test //collection of classes
 {
     internal class Program // collection of variable and functions
     {
         static void Main(string[] args) // code to executed
         {
-            var car1 = new Car("Audi");
-            var car2 = new Car("Honda");
+            Vehicle<int>   v = new Vehicle<int>();
+            v.PrintValue();
+
+            Vehicle<string> v2 = new Vehicle<string>();
+            v2.PrintValue();
+
+            Machine obj = new Machine();
+            PrintMessage p = new PrintMessage(obj.PrintValue);
+            p("Saif");
+
+            Func<int, int, int> add = (x, y) => x + y;
+
+            Console.WriteLine(add(3,4));
+
+            Console.ReadLine();
+
+        }
+
+        
+
+        public delegate void PrintMessage(string message);
+
+        public class Machine
+        {
+            public void PrintValue(string value)
+            {
+                Console.WriteLine(value);
+            }
         }
     }
 }
